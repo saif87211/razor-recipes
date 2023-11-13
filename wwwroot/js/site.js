2a16b675a2afba957810c7d9d5e44e7acf1c52a5
Saif Ali <saifali87211@gmail.com>
2023-11-13 21:53:06 +0530

    Add Slick Sliders

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $('#main-slider').slick({
+        adaptiveHeight:true,
+        autoplay:true,
+        centerMode:true,
+        centerPadding: '10px',
+        dots:true,
+        draggable:true,
+        fade:true,
+        infinite:true,
+        mobileFirst:true,
+        pauseOnFocus:false,
+    });
+
+    $('#category-slider').slick({
+        autoplay:true,
+        infinite:true,
+        slidesToShow: 6,
+    });
+});
